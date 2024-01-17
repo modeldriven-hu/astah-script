b5b2f069d72c4a96d2827578c9ba0bed651b0677
Zsolt Sandor <zsolt.sandor@gmail.com>
2024-01-17 20:58:47 +0100

    Initial commit

---
+++ src/test/java/hu/modeldriven/astah/script/TemplateActionTest.java
@@ -0,0 +1,17 @@
+package hu.modeldriven.astah.script;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TemplateActionTest {
+	
+	@Test
+	public void testname() throws Exception {
+		
+	}
+
+}
+
+
